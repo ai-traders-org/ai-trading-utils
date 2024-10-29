5a0c6c6e8874085ddc2607aeb99f47cce334ff22
bartoszbok <bartosz.bok@nask.pl>
2024-10-29 10:59:50 +0100

    add basic files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_trading_utils",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 'dependency1',
+        # 'dependency2',
+    ],
+    author="Bartosz Bok",
+    author_email="bok.bartosz@gmail.com",
+    description="utils for `ai-trading` project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ai-traders-org/ai-trading-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.10',
+)
